4b2b138d580629e55c4ab28ce2f568554d42b8bd
Young Geun Kim <dudrmshub@gmail.com>
2022-01-31 16:40:01 +0900

    add accepted sac 2022 paper

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:/Users/ygeunkim/Documents/GitHub/young-blogdown/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
+  // ns-hugo:/Users/ygeunbook/Documents/GitHub/young-blogdown/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
